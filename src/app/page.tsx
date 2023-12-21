08d664970ec1550be3a2cf7b0e7d731034801e15
Scrice <davidemassimo94@gmail.com>
2023-12-21 18:05:25 +0100

    ci: testing pipeline

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,9 +8,8 @@ export default function Home() {
     <> 
       <h1>Home Page</h1>
       <ShowLoggedUser />
-      <Link href="/authentication/login">Log In to enter in the App</Link>
       <hr></hr>
-      <Link href="/todolist">Enter in app if already logged in</Link>
+      <Link href="/todolist">Enter in the app</Link>
     </>
   )
 }
